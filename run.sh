b5583915630dc46ea38a9c0b2b765dc0950b29cf
David Rhodus <david.rhodus859@gmail.com>
2018-12-01 12:22:17 -0500

    add 9999 ports to docker run command

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -ti --rm --name clevis -p 3000:3000 -p 8545:8545 -p9999:9999 -v ${PWD}:/dapp austingriffith/clevis
+docker run -ti --rm --name clevis -p 9999:9999 -p 3000:3000 -p 8545:8545 -v ${PWD}:/dapp austingriffith/clevis
